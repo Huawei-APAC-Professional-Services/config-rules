08f3a7c0078d1517a949529a9478d8646527da77
mu <agoodmu@gmail.com>
2023-11-13 17:48:22 +0800

    add check info

--- service/iam.go
+++ service/iam.go
@@ -2,6 +2,7 @@ package service
 
 import (
 	"errors"
+	"log/slog"
 
 	"github.com/huaweicloud/huaweicloud-sdk-go-v3/core/auth/global"
 	iam "github.com/huaweicloud/huaweicloud-sdk-go-v3/services/iam/v3"
@@ -26,9 +27,11 @@ func (i *ConfigIAMClient) HasOnlyOneEnterpriseAdministrator() (bool, error) {
 	if err != nil {
 		return false, err
 	}
+	slog.Info("Check admin group id")
 	if len(*groupqueryResult.Groups) != 1 {
 		return false, errors.New("there is more than one admin group")
 	}
+	slog.Info("Get admin group id")
 	adminGroupId := (*groupqueryResult.Groups)[0].Id
 	groupusersResult, err := i.client.KeystoneListUsersForGroupByAdmin(&model.KeystoneListUsersForGroupByAdminRequest{GroupId: adminGroupId})
 	if len(*groupusersResult.Users) <= 1 {
